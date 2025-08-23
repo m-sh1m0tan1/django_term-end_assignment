a7ba84c96a211860b7b9f98c5a94d34e4ceddcd4
masaya <m.shimotani.sys24@morijyobi.ac.jp>
2025-08-23 15:48:29 +0900

    試み10th

--- config/settings.py
+++ config/settings.py
@@ -29,9 +29,9 @@ env.read_env(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['django-term-end-assignment-application.onrender.com']
 
 
 # Application definition
