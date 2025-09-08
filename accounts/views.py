2ec6c740a881cd93012e9a009fe0e4095c287ef2
masaya <m.shimotani.sys24@morijyobi.ac.jp>
2025-09-08 13:06:54 +0900

    新規登録した際にリダイレクトされるURLの不備の修正

--- accounts/views.py
+++ accounts/views.py
@@ -5,28 +5,32 @@ from django.contrib.auth.views import LoginView
 from django.core.exceptions import PermissionDenied
 
 from .forms import CustomUserCreationForm
+
 # Create your views here.
+
 
 class SignUpView(generic.CreateView):
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'accounts/signup.html'
-    
+    success_url = reverse_lazy("attendance:ModelListView")
+    template_name = "accounts/signup.html"
+
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
-        kwargs['user'] = self.request.user
+        kwargs["user"] = self.request.user
         return kwargs
-    
+
+
 class CustomLoginView(LoginView):
     def get_success_url(self):
         # print('get_success_url')
         user = self.request.user
         if user.role == 1:
             # print('先生')
-            return reverse_lazy('attendance:ModelListView')
+            return reverse_lazy("attendance:ModelListView")
         elif user.role == 2:
-            return reverse_lazy('attendance:StudentAttendView')
-        raise PermissionDenied('不正なユーザーです。')
-    
+            return reverse_lazy("attendance:StudentAttendView")
+        raise PermissionDenied("不正なユーザーです。")
+
+
 class StudentSignupView(generic.CreateView):
-    template_name = 'accounts/signup_students.html'
+    template_name = "accounts/signup_students.html"
